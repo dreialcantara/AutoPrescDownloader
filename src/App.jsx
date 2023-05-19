dac92cc504024fe70bb311bc80b416e844cf6e7b
dreialcantara <andreialcantara@id.uff.br>
2023-05-19 19:09:53 -0300

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import "./App.css";
 function App() {
   return (
     <div className="mt-1 w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-      <img className="img-fluid" src="/src/assets/logo.png" alt="" />
+      <img
+        className="img-fluid"
+        src="https://ai-jobs.net/media/company/logo/22/04/MenofManual.png"
+        alt=""
+      />
       <p className="display-4 header">PHARMACY DOWNLOAD MANAGER</p>
       <div className="w-50 d-flex justify-content-between align-items-center mt-3">
         <Presc />
